2a31a8bb64e5ff8bf927dde7a85470e0729f6d49
VyomUnadkat <18560093+VyomUnadkat@users.noreply.github.com>
2020-06-11 00:09:25 +0530

    initial commit

--- map/ViewController.swift
+++ map/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
 
+    @IBOutlet weak var map: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let latitude: CLLocationDegrees = 34.0224
+        
+        let longitude: CLLocationDegrees = -118.2851
+        
+        let latdelta: CLLocationDegrees = 0.05
+        
+        let longdelta: CLLocationDegrees = 0.05
+        
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: latdelta, longitudeDelta: longdelta)
+        
+        let coordinate: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+        
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: coordinate, span: span)
+        
+        map.setRegion(region, animated: true)
+        
+        
+        let annotation = MKPointAnnotation()
+        
+        annotation.title = "The United States America"
+        
+        annotation.subtitle = "Been here"
+        
+        annotation.coordinate = coordinate
+        
+        map.addAnnotation(annotation)
+        
+        
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +53,6 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
 }
 
